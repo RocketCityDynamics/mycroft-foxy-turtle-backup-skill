2dbbe2da8d7d734d692f686c768c0d6f106d928d
Rocket City Dynamics <46632364+RocketCityDynamics@users.noreply.github.com>
2021-02-26 19:27:41 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import subprocess
 
 class MycroftFoxyTurtlesimTurtleBackup(MycroftSkill):
     def __init__(self):
@@ -7,8 +7,8 @@ class MycroftFoxyTurtlesimTurtleBackup(MycroftSkill):
 
     @intent_file_handler('backup.turtle.turtlesim.foxy.mycroft.intent')
     def handle_backup_turtle_turtlesim_foxy_mycroft(self, message):
-    self.speak_dialog('backup.turtle.turtlesim.foxy.mycroft')
-
+        self.speak_dialog('backup.turtle.turtlesim.foxy.mycroft')
+        subprocess.call(["ros2 topic pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist '{linear: {x: -2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}'"],shell=True)
 
 def create_skill():
     return MycroftFoxyTurtlesimTurtleBackup()
